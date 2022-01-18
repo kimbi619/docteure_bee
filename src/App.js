ab3bf2d6770533445dd02e4494759edd90083dd3
kimbi619 <kimbidarl@gmail.com>
2022-01-17 18:33:33 -0800

    remove comments to make app visible

--- src/App.js
+++ src/App.js
@@ -16,25 +16,25 @@ function App() {
   console.log(app);
 
 
-  // const [products, setProducts] = useLocalStorage('products', [])
-  // useEffect(() => {  
-  //   Client.getEntries()
-  //   .then(res=>{
-  //     setProducts(res.items);
-  //   })
-  //   .catch(err=>console.log(err))
+  const [products, setProducts] = useLocalStorage('products', [])
+  useEffect(() => {  
+    Client.getEntries()
+    .then(res=>{
+      setProducts(res.items);
+    })
+    .catch(err=>console.log(err))
 
-  // }, []);
+  }, []);
 
-  // const handleFilter = (category)=>{
-  //   console.log(category.target);
-  // }
+  const handleFilter = (category)=>{
+    console.log(category.target);
+  }
 
 
 
   return (
     <div className="App">
-      {/* <Nav products={products} />
+      <Nav products={products} />
       <Route path="/products" exact><CategoryCarousel onFilter={handleFilter} products={products} /></Route>
       <Main products={products} />
       
@@ -42,7 +42,7 @@ function App() {
         <Route path="/register/signup" component={Register}  />
         <Route path="/register/login" component={Register}  />
         <Route to="/" exact><HomePage products={products} /></Route>
-      </Switch> */}
+      </Switch>
     </div>
   );
 }
