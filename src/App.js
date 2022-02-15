124d979577d114f93ec7aa3ae1720bfd322cda18
kimbi619 <kimbidarl@gmail.com>
2022-02-14 20:41:23 -0800

    remove con

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
   const [products, setProducts] = useLocalStorage('products', [])
   useEffect((locale = 'en-US') => {  
     Client.getEntries()
-    locale = locale
+    // locale = locale
     .then(res=>{
       setProducts(res.items);
     })
